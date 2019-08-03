40fa61fff556e8cd7ae27f5b62035b38e6ac7b5b
Neyasbltb <neyasbltb_88@mail.ru>
2019-08-03 19:58:55 +0300

    Демонстрация работы на демо-странице по наведению на элементы

--- scripts/common.js
+++ scripts/common.js
@@ -11,3 +11,25 @@ path = findFullPath.find(item);
 console.log(path);
 
 console.log(document.querySelectorAll(path));
+let path_info = document.querySelector('.path_info');
+
+document.addEventListener('mouseover', e => {
+    let path = findFullPath.find(e.target, false);
+    let target_build_path = findFullPath._buildPath([path.pop()]);
+    let parent_build_path = findFullPath._buildPath(path);
+
+    path_info.innerHTML = `${parent_build_path} <span class="highlight_target">${target_build_path}</span>`
+
+    let style = document.querySelector('.findFullPath_highlight');
+    if (!style) {
+        style = document.createElement('style');
+        style.className = 'findFullPath_highlight';
+        document.head.appendChild(style);
+    }
+
+    style.textContent = /* css */ `
+        ${parent_build_path + ' ' + target_build_path} {
+            outline: 1px solid #f00;
+        }
+    `;
+});
