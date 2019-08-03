5cb013219fb1268adfaccab257d78894c00b67f9
Neyasbltb <neyasbltb_88@mail.ru>
2019-08-03 20:05:13 +0300

    README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Find Full Path
+Скрипт для определения полного уникального CSS-селектора переданного элемента.
+
+## [Демо-страница](https://neyasbltb88.github.io/FindFullPath/index.html)
--- scripts/common.js
+++ scripts/common.js
@@ -2,15 +2,6 @@ import FindFullPath from "./FindFullPath.js";
 
 window.findFullPath = new FindFullPath();
 
-window.item = document.querySelectorAll(".board_body_item")[3];
-
-let path = findFullPath.find(item, false);
-console.log(path);
-
-path = findFullPath.find(item);
-console.log(path);
-
-console.log(document.querySelectorAll(path));
 let path_info = document.querySelector('.path_info');
 
 document.addEventListener('mouseover', e => {
